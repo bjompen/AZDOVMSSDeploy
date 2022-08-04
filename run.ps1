33339c350cbb0bc810d496928fc28d3926baef7b
Björn Sundling <bjorn.sundling@advania.com>
2022-08-04 12:07:03 +0200

    add service principal

--- run.ps1
+++ run.ps1
@@ -6,6 +6,9 @@ $VMSSName ='myvmss'
 
 $sshPubKey = Get-Content C:\Users\bjompen\.ssh\id_rsa.pub
 
+## Deploy resources
+Connect-AzAccount
+
 $rg = New-AzResourceGroup -Name $RGName -Location $Location
 
 $vnet = New-AzResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName -TemplateFile .\vnet.bicep -TemplateParameterObject @{
@@ -20,4 +23,30 @@ $vmss = New-AzResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName -
     subnetId = $subId
 } -Verbose
 
-## Set it up in AZDO
+## Create service account
+$AzureADIdentityDisplayName = 'MyBicepADOServiceConnectionIdentity'
+
+$AppIdentity = New-AzADApplication -DisplayName $AzureADIdentityDisplayName 
+$AppIdentityPass = New-AzADAppCredential -ObjectId $AppIdentity.id
+$SecurePassword = ConvertTo-SecureString $AppIdentityPass.SecretText -AsPlainText -Force
+
+## Grant service account access to the new resource group
+
+
+
+## Service connection in azdo
+$Context = Get-AzContext
+
+Connect-ADOPS -Username 'bjorn.sundling@gmail.com' -Organization 'bjornsundling' -PersonalAccessToken $PAT
+
+$AzureServicePrincipal = [pscredential]::new($AppIdentity.AppId, $SecurePassword)
+
+$ServiceConenctionSplat = @{
+    TenantId = $Context.Tenant.Id 
+    SubscriptionId = $Context.Subscription.Id 
+    SubscriptionName = $Context.Subscription.Name 
+    Project = 'LabProject' 
+    ConnectionName = 'MyBicepConnection'
+    ServicePrincipal = $AzureServicePrincipal
+}
+New-ADOPSServiceConnection @ServiceConenctionSplat
